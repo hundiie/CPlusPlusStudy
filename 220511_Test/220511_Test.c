ccba48e5b749d49a4ebe3a3327214c590d3896e0
leedahun <ldh1923@naver.com>
2022-05-11 17:49:50 +0900

    220511_Study
    끄적인거

---
+++ 220511_Test/220511_Test.c
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<Windows.h>
+#include<stdlib.h>
+
+void TextColer(int a, int b)
+{
+	a %= 16;
+	b %= 16;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (a + (b * 16)));
+}
+	int main(void)
+{
+		srand(time(NULL));
+		//for (int i = 0; i < 255; i++)
+		//{
+		//	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i);
+		//	printf(" 글자색 %d\n", i);
+		//}
+	while (1)
+	{
+		int a;
+		int b;
+
+		
+		printf("글자색 : ");
+		scanf_s("%d", &a);
+		printf("배경색 : ");
+		scanf_s("%d", &b);
+
+
+
+
+		TextColer(a, b);
+
+		printf("글자색 %d  배경색 %d\n", a, b);
+	}
+}
