514b03c9eb72265b15bc619ebaaf32c21f2e5d0e
LDH <ldh1923@naver.com>
2022-05-30 18:23:41 +0900

    220530_Study
    객체지향, 클래스

---
+++ 220530_Study/220530_Study/220530_Study.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+class test
+{
+public://private: 사용 시 컴파일 에러
+
+	test()//생성자
+	{
+		printf("hello ");
+	}
+	~test()//소멸자
+	{
+		printf("world!");
+	}
+};
+
+int main()
+{
+	test a;
+}
